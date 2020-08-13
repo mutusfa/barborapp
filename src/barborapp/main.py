90ebf21e4fa4c0123912bc2316cd221da71efb41
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2020-08-13 23:50:33 +0300

    Create weekly summary with order counts data

--- src/barborapp/main.py
+++ src/barborapp/main.py
@@ -8,6 +8,7 @@ from pyspark.sql.utils import AnalysisException
 from pyspark.sql.types import DataType
 
 from barborapp.typing import AMOUNT_TYPE
+from barborapp import aggregates
 
 
 def hello():
@@ -15,6 +16,43 @@ def hello():
 
 
 LOG = logging.getLogger(__name__)
+
+# Utils ========================================================================
+
+
+def cast_columns(
+    dataframe: DataFrame, columns: List[str], type_: Union[DataType, str]
+) -> DataFrame:
+    for col_name in columns:
+        try:
+            dataframe = dataframe.withColumn(
+                col_name, F.col(col_name).cast(type_)
+            )
+        except AnalysisException:
+            LOG.warning("%s column not found", col_name)
+    return dataframe
+
+
+def rename_and_fill_columns(
+    df: DataFrame, rename: dict, fill_value=None, add_missing_columns=True,
+) -> DataFrame:
+    if add_missing_columns:
+        for column in rename:
+            try:
+                df[column]  # just to raise an exception if column doesn't exist
+            except AnalysisException:
+                df = df.withColumn(column, F.lit(fill_value))
+    df = df.na.fill(fill_value)
+    columns_to_keep = [
+        col_name for col_name in df.columns if col_name not in rename
+    ]
+    select_args = columns_to_keep + [
+        F.col(orig).alias(new) for orig, new in rename.items()
+    ]
+    return df.select(select_args)
+
+
+# Loading of data ==============================================================
 
 
 def _period_start(date_str):
@@ -31,19 +69,6 @@ period_start = F.udf(_period_start)
 
 def add_periods(dataframe: DataFrame, date_col: str = "dt") -> DataFrame:
     return dataframe.withColumn("period_start", period_start(date_col))
-
-
-def cast_columns(
-    dataframe: DataFrame, columns: List[str], type_: Union[DataType, str]
-) -> DataFrame:
-    for col_name in columns:
-        try:
-            dataframe = dataframe.withColumn(
-                col_name, F.col(col_name).cast(type_)
-            )
-        except AnalysisException:
-            LOG.warning("%s column not found", col_name)
-    return dataframe
 
 
 def load_data(
@@ -69,3 +94,30 @@ def load_data(
     df = cast_columns(df, int_columns, "integer")
     df = add_periods(df, "dt")
     return df
+
+
+# Aggregation ==================================================================
+
+
+def get_weekly_aggregates(dataframe: DataFrame) -> DataFrame:
+    grouped_data = dataframe.groupby(["customer_id", "period_start"])
+    weekly_summary_df = grouped_data.count().select(
+        ["customer_id", "period_start"]
+    )
+    order_counts = aggregates.get_order_count_by_type(grouped_data)
+    order_counts = rename_and_fill_columns(
+        order_counts,
+        {
+            "Delivered": "order_executed_count",
+            "Placed": "order_placed_count",
+            "Returned": "order_returned_count",
+        },
+        fill_value=0,
+    )
+    order_counts.show()
+    weekly_summary_df = weekly_summary_df.join(
+        order_counts,
+        (weekly_summary_df.customer_id == order_counts.customer_id)
+        & (weekly_summary_df.period_start == order_counts.period_start),
+    )
+    return weekly_summary_df
