299733168c129fa31df8238432801ba56d7be4b7
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2020-08-14 07:19:32 +0300

    Add sanity tests for _add_missing_columns

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import unittest
 
+from pyspark.sql.types import StringType
+
 import barborapp.main
 
 from pyspark_testcase import PySparkTestCase
@@ -43,3 +45,39 @@ class TestLoadData(PySparkTestCase):
                 ]
             ),
         )
+
+
+class TestAddMissingColumns(PySparkTestCase):
+    def test_missing_columns(self):
+        # given
+        inv_dt = ["2018-09-{:02d}".format(x) for x in range(1, 31)]
+        df = self.spark.createDataFrame(inv_dt, StringType()).withColumnRenamed(
+            "value", "inv_dt"
+        )
+        self.assertSetEqual(set(df.columns), set(["inv_dt"]))
+
+        # when
+        df = barborapp.main._add_missing_columns(
+            df, ["inv_dt", "cheese"], fill_value="ham", type_=StringType()
+        )
+
+        # then
+        self.assertSetEqual(set(df.columns), set(["inv_dt", "cheese"]))
+        self.assertEqual(df.first().cheese, "ham")
+
+    def test_no_missing_columns(self):
+        # given
+        inv_dt = ["2018-09-{:02d}".format(x) for x in range(1, 31)]
+        df = self.spark.createDataFrame(inv_dt, StringType()).withColumnRenamed(
+            "value", "inv_dt"
+        )
+        self.assertSetEqual(set(df.columns), set(["inv_dt"]))
+
+        # when
+        df = barborapp.main._add_missing_columns(
+            df, ["inv_dt"], fill_value="ham", type_=StringType()
+        )
+
+        # then
+        self.assertSetEqual(set(df.columns), set(["inv_dt"]))
+        self.assertNotEqual(df.first().inv_dt, "ham")
